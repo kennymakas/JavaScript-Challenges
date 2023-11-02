a56e9c0fc2a68ff8fa344b5684640b5b9d25f511
kennymakas Ian <kennymakas@gnail.com>
2023-11-02 20:42:10 +0300

    first

---
+++ set one qtn1.js
@@ -0,0 +1,15 @@
+function fizzBuzz(strg1, strg2){
+    var combine_length = strg1.length + strg2.length;
+
+    if (combine_length % 3 === 0 && combinedLength % 5 === 0){
+        return "FizzBuzz";
+} else if (combinedLength % 3 === 0){
+    return "Fizz";
+
+} else if (combinedLength % 5 === 0) {
+    return "Buzz";
+
+}else {
+    return combinedLength.tostring();
+}
+}
